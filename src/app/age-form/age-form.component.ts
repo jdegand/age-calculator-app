02e6316c1caf8086ecf1819e95f036d13b2db307
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-07-25 02:43:42 +0000

    fix: format and remove unused code in age form ts

--- src/app/age-form/age-form.component.ts
+++ src/app/age-form/age-form.component.ts
@@ -3,37 +3,6 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { realDate } from '../validators/RealDateValidator';
 import intervalToDuration from 'date-fns/intervalToDuration';
 
-/*
-import {
-  trigger,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
-
-const enterTransition = transition(':enter', [
-  style({ 
-    opacity: 0
-  }),
-  animate('1s ease-in', style({opacity: 1}))
-])
-
-const fadeIn = trigger('fadeIn', [enterTransition]);
-
-// animations: [fadeIn]
-*/
-/*
-trigger('myInsertRemoveTrigger', [
-  transition(':enter', [
-    style({ opacity: 0 }),
-    animate('100ms', style({ opacity: 1 })),
-  ]),
-  transition(':leave', [
-    animate('100ms', style({ opacity: 0 }))
-  ])
-])
-*/
-
 @Component({
   selector: 'app-age-form',
   templateUrl: './age-form.component.html',
@@ -44,33 +13,32 @@ export class AgeFormComponent {
     day: new FormControl('', [Validators.required, Validators.min(1), Validators.max(31)]),
     month: new FormControl('', [Validators.required, Validators.min(1), Validators.max(12)]),
     year: new FormControl('', [Validators.required, Validators.min(1900), Validators.max(new Date().getFullYear())])
-  }, {validators: realDate('day', 'month', 'year')});
+  }, { validators: realDate('day', 'month', 'year') });
 
   // make variables for new calculated dates and use those variables in the html
   yearsPassed: number | undefined = undefined;
   monthsPassed: number | undefined = undefined;
-  daysPassed:number | undefined = undefined;
+  daysPassed: number | undefined = undefined;
 
-  onSubmit(){
-    //console.log(this.ageForm);
+  onSubmit() {
 
-    if(this.ageForm.valid){
+    if (this.ageForm.valid) {
       const year = Number(this.ageForm.value.year);
       const month = Number(this.ageForm.value.month);
       const day = Number(this.ageForm.value.day);
 
-    // new Date(year, monthIndex, day, hours, minutes, seconds, milliseconds)
+      // new Date(year, monthIndex, day, hours, minutes, seconds, milliseconds)
 
-    /*
-    intervalToDuration({
-      start: new Date(1929, 0, 15, 12, 0, 0),
-      end: new Date(1968, 3, 4, 19, 5, 0)
-    })
-    */
-    // => { years: 39, months: 2, days: 20, hours: 7, minutes: 5, seconds: 0 }
+      /*
+      intervalToDuration({
+        start: new Date(1929, 0, 15, 12, 0, 0),
+        end: new Date(1968, 3, 4, 19, 5, 0)
+      })
+      */
+      // => { years: 39, months: 2, days: 20, hours: 7, minutes: 5, seconds: 0 }
 
-      const {years, months, days} = intervalToDuration({
-        start: new Date(year, (month-1), day, 0,0,0),
+      const { years, months, days } = intervalToDuration({
+        start: new Date(year, (month - 1), day, 0, 0, 0),
         end: new Date()
       })
 
